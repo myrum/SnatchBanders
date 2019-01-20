02a09a29d17cfb92db6d1acde28a28f36996a0d6
notCandace <dorseymakenzie@yahoo.com>
2019-01-19 18:08:06 -0800

    adding code to github

---
+++ Assets/leScripty.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class leScripty : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((transform != null) && Input.GetKey("right"))
+        {
+            transform.Rotate(0, (float).5, 0);
+        }
+
+        if ((transform != null) && Input.GetKey("left"))
+        {
+            transform.Rotate(0, (float)-.5, 0);
+        }
+
+        if ((transform != null) && Input.GetKey("up"))
+        {
+            transform
+        }
+    }
+}
